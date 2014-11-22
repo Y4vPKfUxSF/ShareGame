76a28da86180484b2d90248e215ef3385eb9f684
hiroshige <hiroshige@myworkbook_001>
2014-11-22 17:01:00 +0900

    add comment

--- src/com/example/sharegame/MainActivity.java
+++ src/com/example/sharegame/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
-    @Override
+    @Override/* aaaaaaaa */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
