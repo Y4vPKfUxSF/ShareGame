495677161c56c582dce96ec0b128c4b2b3e3c8e0
shingo <shingo@shingo-idea->
2015-12-10 21:07:26 +0900

    完成版

--- gen/com/example/sharegame/R.java
+++ gen/com/example/sharegame/R.java
@@ -32,8 +32,7 @@ public final class R {
     public static final class drawable {
         public static final int crabe=0x7f020000;
         public static final int crow=0x7f020001;
-        public static final int gameover=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f080000;
